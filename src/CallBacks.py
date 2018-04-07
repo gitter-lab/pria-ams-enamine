3a390da0a21d6a7b03058bff6e00c245a6a487b2
chao1224 <shengchao.hacker@gmail.com>
2018-04-06 20:31:31 -0500

    Updates: - Remove croc - Remove unused imports - Remove multi-task callback

--- src/CallBacks.py
+++ src/CallBacks.py
@@ -45,14 +45,8 @@ class KeckCallBackOnROC(keras.callbacks.Callback):
             self.model.save_weights(self.file_path)
 
         train_roc = roc_auc_single(self.y_train, self.model.predict(self.X_train))
-        # train_bedroc = bedroc_auc_single(self.y_train, self.model.predict(self.X_train))
         train_pr = precision_auc_single(self.y_train, self.model.predict(self.X_train))
-        # curr_bedroc = bedroc_auc_single(self.y_val, self.model.predict(self.X_val))
         curr_pr = precision_auc_single(self.y_val, self.model.predict(self.X_val))
-        # print 'Train\tAUC[ROC]: %.6f\tAUC[BEDROC]: %.6f\tAUC[PR]: %.6f' % \
-        #       (train_roc, train_bedroc, train_pr)
-        # print 'Val\tAUC[ROC]: %.6f\tAUC[BEDROC]: %.6f\tAUC[PR]: %.6f' % \
-        #       (self.curr_roc, curr_bedroc, curr_pr)
         print 'Train\tAUC[ROC]: %.6f\tAUC[PR]: %.6f' % \
               (train_roc, train_pr)
         print 'Val\tAUC[ROC]: %.6f\tAUC[PR]: %.6f' % \
@@ -112,14 +106,8 @@ class KeckCallBackOnPrecision(keras.callbacks.Callback):
             self.model.save_weights(self.file_path)
 
         train_roc = roc_auc_single(self.y_train, self.model.predict(self.X_train))
-        # train_bedroc = bedroc_auc_single(self.y_train, self.model.predict(self.X_train))
         train_pr = precision_auc_single(self.y_train, self.model.predict(self.X_train))
         curr_roc = roc_auc_single(self.y_val, self.model.predict(self.X_val))
-        # curr_bedroc = bedroc_auc_single(self.y_val, self.model.predict(self.X_val))
-        # print 'Train\tAUC[ROC]: %.6f\tAUC[BEDROC]: %.6f\tAUC[PR]: %.6f' % \
-        #       (train_roc, train_bedroc, train_pr)
-        # print 'Val\tAUC[ROC]: %.6f\tAUC[BEDROC]: %.6f\tAUC[PR]: %.6f' % \
-        #       (curr_roc, curr_bedroc, self.curr_pr)
         print 'Train\tAUC[ROC]: %.6f\tAUC[PR]: %.6f' % \
               (train_roc, train_pr)
         print 'Val\tAUC[ROC]: %.6f\tAUC[PR]: %.6f' % \
@@ -136,164 +124,3 @@ class KeckCallBackOnPrecision(keras.callbacks.Callback):
 
     def get_best_roc(self):
         return self.best_pr
-
-
-
-class MultiCallBackOnROC(keras.callbacks.Callback):
-    def __init__(self, X_train, y_train, X_val, y_val,
-                 eval_indices, eval_mean_or_median, patience=0,
-                 file_path='best_model.weights'):
-        super(keras.callbacks.Callback, self).__init__()
-        self.curr_roc = 0
-        self.best_roc = 0
-        self.counter = 0
-        self.patience = patience
-        self.best_model = None
-        self.eval_indices = eval_indices
-        self.eval_mean_or_median = eval_mean_or_median
-        self.X_train = X_train
-        self.y_train = y_train
-        self.X_val = X_val
-        self.y_val = y_val
-        self.file_path = file_path
-
-    def on_train_begin(self, logs={}):
-        self.nb_epoch = self.params['nb_epoch']
-        self.curr_roc = self.get_model_roc_auc(self.y_val, self.model.predict(self.X_val))
-        self.best_roc = self.curr_roc
-        self.model.save_weights(self.file_path)
-        self.time = time.time()
-
-    def on_epoch_end(self, epoch, logs={}):
-        print('Epoch {}/{}'.format(epoch + 1, self.nb_epoch))
-        training_end_time = time.time()
-        print('Epoch training duration: {}'.format(training_end_time-self.time))
-        y_pred_val = self.model.predict(self.X_val)
-        y_pred_train = self.model.predict(self.X_train)
-        self.curr_roc = self.get_model_roc_auc(self.y_val, y_pred_val)
-        if self.curr_roc < self.best_roc:
-            if self.counter >= self.patience:
-                self.model.stop_training = True
-            else:
-                self.counter += 1
-        else:
-            self.counter = 0
-            self.best_roc = self.curr_roc
-            self.model.save_weights(self.file_path)
-
-        curr_pr = self.get_model_precision_auc(self.y_val, y_pred_val)
-        # curr_bedroc = self.get_model_bedroc_auc(self.y_val, y_pred_val)
-        train_roc = self.get_model_roc_auc(self.y_train, y_pred_train)
-        # train_bedroc = self.get_model_bedroc_auc(self.y_train, y_pred_train)
-        train_pr = self.get_model_precision_auc(self.y_train, y_pred_train)
-        # print 'Train\tAUC[ROC]: %.6f\tAUC[BEDROC]: %.6f\tAUC[PR]: %.6f' % \
-        #       (train_roc, train_bedroc, train_pr)
-        # print 'Val\tAUC[ROC]: %.6f\tAUC[BEDROC]: %.6f\tAUC[PR]: %.6f' % \
-        #       (self.curr_roc, curr_bedroc, curr_pr)
-        print 'Train\tAUC[ROC]: %.6f\tAUC[PR]: %.6f' % \
-              (train_roc, train_pr)
-        print 'Val\tAUC[ROC]: %.6f\tAUC[PR]: %.6f' % \
-              (self.curr_roc, curr_pr)
-        end_time = time.time()
-        print('Epoch evaluation duration: {}'.format(end_time-training_end_time))
-        print('Epoch duration: {}'.format(end_time-self.time))
-        self.time = end_time
-        print
-
-    def get_best_model(self):
-        self.model.load_weights(self.file_path)
-        return self.model
-
-    def get_best_roc(self):
-        return self.best_roc
-
-    def get_model_roc_auc(self, true_label, predicted_label):
-        return roc_auc_multi(true_label, predicted_label, self.eval_indices, self.eval_mean_or_median)
-
-    def get_model_bedroc_auc(self, true_label, predicted_label):
-        return bedroc_auc_multi(true_label, predicted_label, self.eval_indices, self.eval_mean_or_median)
-
-    def get_model_precision_auc(self, true_label, predicted_label):
-        return precision_auc_multi(true_label, predicted_label, self.eval_indices, self.eval_mean_or_median)
-
-
-class MultiCallBackOnPR(keras.callbacks.Callback):
-    # define custom classes
-    # following class is used for keras to compute the precision each epoch
-    # and do early stoppping based on that
-    def __init__(self, X_train, y_train, X_val, y_val,
-                 eval_indices, eval_mean_or_median, patience=0,
-                 file_path='best_model.weights'):
-        super(keras.callbacks.Callback, self).__init__()
-        self.curr_pr = 0
-        self.best_pr = 0
-        self.counter = 0
-        self.patience = patience
-        self.best_model = None
-        self.eval_indices = eval_indices
-        self.eval_mean_or_median = eval_mean_or_median
-        self.X_train = X_train
-        self.y_train = y_train
-        self.X_val = X_val
-        self.y_val = y_val
-        self.file_path = file_path
-
-    def on_train_begin(self, logs={}):
-        self.nb_epoch = self.params['nb_epoch']
-        self.curr_pr = self.get_model_precision_auc(self.y_val, self.model.predict(self.X_val))
-        self.best_pr = self.curr_pr
-        self.model.save_weights(self.file_path)
-        self.time = time.time()
-
-    def on_epoch_end(self, epoch, logs={}):
-        print('Epoch {}/{}'.format(epoch + 1, self.nb_epoch))
-        training_end_time = time.time()
-        print('Epoch training duration: {}'.format(training_end_time-self.time))
-        y_pred_val = self.model.predict(self.X_val)
-        y_pred_train = self.model.predict(self.X_train)
-        self.curr_pr = self.get_model_precision_auc(self.y_val, y_pred_val)
-        if self.curr_pr < self.best_pr:
-            if self.counter >= self.patience:
-                self.model.stop_training = True
-            else:
-                self.counter += 1
-        else:
-            self.counter = 0
-            self.best_pr = self.curr_pr
-            self.model.save_weights(self.file_path)
-
-        curr_roc = self.get_model_roc_auc(self.y_val, y_pred_val)
-        # curr_bedroc = self.get_model_bedroc_auc(self.y_val, y_pred_val)
-        train_roc = self.get_model_roc_auc(self.y_train, y_pred_train)
-        # train_bedroc = self.get_model_bedroc_auc(self.y_train, y_pred_train)
-        train_pr = self.get_model_precision_auc(self.y_train, y_pred_train)
-
-        # print 'Train\tAUC[ROC]: %.6f\tAUC[BEDROC]: %.6f\tAUC[PR]: %.6f' % \
-        #       (train_roc, train_bedroc, train_pr)
-        # print 'Val\tAUC[ROC]: %.6f\tAUC[BEDROC]: %.6f\tAUC[PR]: %.6f' % \
-        #       (curr_roc, curr_bedroc, self.curr_pr)
-        print 'Train\tAUC[ROC]: %.6f\tAUC[PR]: %.6f' % \
-              (train_roc, train_pr)
-        print 'Val\tAUC[ROC]: %.6f\tAUC[PR]: %.6f' % \
-              (curr_roc, self.curr_pr)
-        end_time = time.time()
-        print('Epoch evaluation duration: {}'.format(end_time-training_end_time))
-        print('Epoch duration: {}'.format(end_time-self.time))
-        self.time = end_time
-        print
-
-    def get_best_model(self):
-        self.model.load_weights(self.file_path)
-        return self.model
-
-    def get_best_pr(self):
-        return self.best_pr
-
-    def get_model_roc_auc(self, true_label, predicted_label):
-        return roc_auc_multi(true_label, predicted_label, self.eval_indices, self.eval_mean_or_median)
-
-    def get_model_bedroc_auc(self, true_label, predicted_label):
-        return bedroc_auc_multi(true_label, predicted_label, self.eval_indices, self.eval_mean_or_median)
-
-    def get_model_precision_auc(self, true_label, predicted_label):
-        return precision_auc_multi(true_label, predicted_label, self.eval_indices, self.eval_mean_or_median)
--- src/evaluation.py
+++ src/evaluation.py
@@ -2,43 +2,8 @@ import pandas as pd
 import csv
 import json
 import numpy as np
-from sklearn.metrics import roc_auc_score, average_precision_score, precision_recall_curve, roc_curve
-from sklearn.metrics import auc
-from croc import BEDROC, ScoredData
+from sklearn.metrics import roc_auc_score, average_precision_score
 import os
-import matplotlib
-import matplotlib.pyplot as plt
-
-
-def roc_auc_multi(y_true, y_pred, eval_indices, eval_mean_or_median,
-                  return_df=False, label_names=None):
-    
-    y_true = y_true[:, eval_indices]
-    y_pred = y_pred[:, eval_indices]
-    nb_classes = y_true.shape[1]
-    auc = np.zeros(nb_classes)
-    for i in range(len(auc)):
-        # -1 represents missing value
-        # and remove them when in evaluation
-        non_missing_indices = np.argwhere(y_true[:, i] != -1)[:, 0]
-        actual = y_true[non_missing_indices, i]
-        predicted = y_pred[non_missing_indices, i]
-        auc[i] = roc_auc_single(actual, predicted)
-    
-    if return_df == True:
-        if label_names == None:
-            label_names = ['label ' + str(i) for i in range(nb_classes)]
-        
-        auc_data = np.concatenate((auc,
-                                   np.mean(auc).reshape(1,),
-                                   np.median(auc).reshape(1,)))
-        auc_df = pd.DataFrame(data=auc_data.reshape(1,len(auc_data)),
-                              index=['ROC AUC'],
-                              columns=label_names+['Mean','Median'])
-        auc_df.index.name='metric'
-        return auc_df
-    else:
-        return eval_mean_or_median(auc)
 
 
 def roc_auc_single(predicted, actual):
@@ -48,77 +13,6 @@ def roc_auc_single(predicted, actual):
         auc_ret = np.nan
     
     return auc_ret
-
-
-def bedroc_auc_multi(y_true, y_pred, eval_indices, eval_mean_or_median,
-                     return_df=False, label_names=None):
-    y_true = y_true[:, eval_indices]
-    y_pred = y_pred[:, eval_indices]
-    nb_classes = y_true.shape[1]
-    auc = np.zeros(nb_classes)
-    for i in range(len(auc)):
-        # -1 represents missing value
-        # and remove them when in evaluation
-        non_missing_indices = np.argwhere(y_true[:, i] != -1)[:, 0]
-        actual = y_true[non_missing_indices, i:(i+1)]
-        predicted = y_pred[non_missing_indices, i:(i+1)]
-        auc[i] = bedroc_auc_single(actual, predicted)
-    
-    if return_df == True:
-        if label_names == None:
-            label_names = ['label ' + str(i) for i in range(nb_classes)]
-        
-        auc_data = np.concatenate((auc,
-                                   np.mean(auc).reshape(1,),
-                                   np.median(auc).reshape(1,)))        
-        auc_df = pd.DataFrame(data=auc_data.reshape(1,len(auc_data)),
-                              index=['BEDROC AUC'],
-                              columns=label_names+['Mean','Median'])
-        auc_df.index.name='metric'
-        return auc_df
-    else:    
-        return eval_mean_or_median(auc)
-
-
-def bedroc_auc_single(predicted, actual, alpha=10):
-    try:
-        data = np.hstack((predicted, actual))
-        data = ScoredData(data)
-        results = BEDROC(data, alpha)
-        return results['area']
-    except:
-        return np.nan
-
-
-def precision_auc_multi(y_true, y_pred, eval_indices, eval_mean_or_median,
-                        mode='auc.integral',
-                        return_df=False, label_names=None):
-    y_true = y_true[:, eval_indices]
-    y_pred = y_pred[:, eval_indices]
-    nb_classes = y_true.shape[1]
-    auc = np.zeros(nb_classes)
-    for i in range(len(auc)):
-        # -1 represents missing value
-        # and remove them when in evaluation
-        non_missing_indices = np.argwhere(y_true[:, i] != -1)[:, 0]
-        actual = y_true[non_missing_indices, i]
-        predicted = y_pred[non_missing_indices, i]
-        auc[i] = precision_auc_single(actual, predicted, mode)
-    
-    if return_df == True:
-        if label_names == None:
-            label_names = ['label ' + str(i) for i in range(nb_classes)]
-        
-        auc_data = np.concatenate((auc,
-                                   np.mean(auc).reshape(1,),
-                                   np.median(auc).reshape(1,))) 
-        auc_df = pd.DataFrame(data=auc_data.reshape(1,len(auc_data)),
-                                  index=['PR ' + mode],
-                                  columns=label_names+['Mean','Median'])
-        auc_df.index.name='metric'
-        return auc_df
-    else:
-        return eval_mean_or_median(auc)
 
 
 def precision_auc_single(predicted, actual):
@@ -150,18 +44,6 @@ def ratio_of_hit_single(predicted, actual, R):
     return number_of_hit_single(actual, predicted, N)
 
 
-def enrichment_factor_multi(actual, predicted, percentile, eval_indices):
-    actual = actual[:, eval_indices]
-    predicted = predicted[:, eval_indices]
-    nb_classes = actual.shape[1]
-    EF_list = []
-    for i in range(nb_classes):
-        n_actives, ef, ef_max = enrichment_factor_single(predicted[:, i], actual[:, i], percentile)
-        temp = [n_actives, ef]
-        EF_list.append(temp)
-    return EF_list
-
-
 def enrichment_factor_single(scores_arr, labels_arr, percentile):
     '''
     calculate the enrichment factor based on some upper fraction
@@ -191,4 +73,3 @@ def enrichment_factor_single(scores_arr, labels_arr, percentile):
         ef = 'ND'
         ef_max = 'ND'
     return n_actives, ef, ef_max
-

2041c71d099883dc5e1c20eb4879eca5bb79c2d1
chao1224 <shengchao.hacker@gmail.com>
2018-10-24 20:19:37 -0500

    Add scripts for chtc scripts

---
+++ chtc/run_random_forest_classification.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo Cluster $cluster
+echo Process $process
+echo RunningOn $runningon
+
+echo "Start environment configuration."
+export HOME=$PWD
+
+wget -q –retry-connrefused –waitretry=10 https://repo.continuum.io/archive/Anaconda2-4.3.1-Linux-x86_64.sh
+chmod 777 *
+./Anaconda2-4.3.1-Linux-x86_64.sh -b -p ./anaconda > /dev/null
+export PATH=$PWD/anaconda/bin:$PATH
+echo 'Done installing anaconda'
+chmod 777 *
+
+conda install --yes -c conda-forge scikit-learn  > /dev/null
+conda install --yes -c rdkit rdkit-postgresql > /dev/null
+echo 'Done installing libraries'
+chmod 777 -R ./anaconda
+
+cd zinc/src/
+
+#process_list=(379 605 440 376 140 32 698 622 47 694 729 314 967 431 360 263 910 518 267 203 44 17 960 889 512 872 832 691 645 628 418 373 298 247 153 108 876 783 761 619 528 458 342 331 167 133 106)
+#process=${process_list["$process"]}
+echo process "$process"
+
+
+date
+pyexit=0
+mode=random_forest_classification
+for ix in `seq 0 4`;
+do
+    mkdir -p ../model_weight/cross_validation_keck/"$mode"
+    mkdir -p ../output/cross_validation_keck/"$mode"
+
+    python cross_validation_keck.py \
+    --config_json_file=../config/cross_validation_keck/"$mode"/"$process".json \
+    --weight_file=../model_weight/cross_validation_keck/"$mode"/"$mode"_"$process"_"$ix".pkl \
+    --process_num="$ix" \
+    --model="$mode" > ../output/cross_validation_keck/"$mode"/"$mode"_"$process"_"$ix".out
+
+    pyexit=$?
+    echo "$pyexit"
+done
+date
+
+cp -r ../output/cross_validation_keck ~/"$transfer_output_files"/
+#cp -r ../model_weight/cross_validation_keck ~/"$transfer_output_files"/
+
+exit $pyexit
---
+++ chtc/run_single_task_classification.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+echo Cluster $cluster
+echo Process $process
+echo RunningOn $runningon
+
+mkdir -p "$transfer_output_files"/cross_validation_keck
+
+
+export HOME=$PWD
+
+export PATH=$PATH:/usr/local/cuda-8.0/bin
+
+wget -q --retry-connrefused --waitretry=10 https://repo.continuum.io/miniconda/Miniconda2-4.2.12-Linux-x86_64.sh
+bash Miniconda2-4.2.12-Linux-x86_64.sh -b -p ./anaconda
+export PATH=$PWD/anaconda/bin:$PATH
+
+
+conda create --yes --name zinc python=2.7 gcc numpy theano=0.8 pandas pygpu keras=1.1 scikit-learn=0.19 matplotlib h5py pyyaml > /dev/null
+source activate zinc
+conda install --yes -c rdkit rdkit-postgresql > /dev/null
+conda install --yes -c anaconda pandas > /dev/null
+conda install --yes -c r rpy2 > /dev/null
+conda install --yes -c bioconda r-prroc=1.1 > /dev/null
+conda install --yes -c auto croc > /dev/null
+conda install -c anaconda cudatoolkit==8.0 --yes > /dev/null
+conda install -c anaconda cudnn==6.0.0 --yes > /dev/null
+
+
+
+wget -nv http://proxy.chtc.wisc.edu/SQUID/agitter/cudnn/cudnn-8.0-linux-x64-v5.0-ga.tgz
+tar -xf cudnn-8.0-linux-x64-v5.0-ga.tgz
+export CUDNN_PATH=$(pwd)/cuda
+export LD_LIBRARY_PATH=$(pwd)/cuda/lib64:/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
+
+# configure keras
+mkdir -p .keras
+echo '{"image_dim_ordering": "th", "epsilon": 1e-07, "floatx": "float32", "backend": "theano"}' > .keras/keras.json
+
+# configure theano
+echo -e "[global]\ndevice = gpu\nfloatX = float32\nbase_compiledir = ./tmp\n\n[gpuarray]\npreallocate = 0.8\n\n[dnn]\nlibrary_path=$CUDNN_PATH/lib64\ninclude_path=$CUDNN_PATH/include\nenabled=True" > .theanorc
+
+nvidia-smi
+
+# Test Keras import
+KERAS_BACKEND=theano python -c "from keras import backend"
+pyexit=$?
+echo "$pyexit"
+
+cd zinc
+tar -xzvf config.tar.gz
+cd src
+
+mode=single_deep_classification
+
+for ix in `seq 0 4`;
+do
+    mkdir -p ../model_weight/cross_validation_keck/"$mode"
+    mkdir -p ../output/cross_validation_keck/"$mode"
+
+    python cross_validation_keck.py \
+    --config_json_file=../config/cross_validation_keck/"$mode"/"$process".json \
+    --weight_file=../model_weight/cross_validation_keck/"$mode"/"$mode"_"$process"_"$ix".pkl \
+    --process_num="$ix" \
+    --model="$mode" > ../output/cross_validation_keck/"$mode"/"$mode"_"$process"_"$ix".out
+
+    pyexit=$?
+    echo "$pyexit"
+done
+date
+
+cp -r ../output/cross_validation_keck ~/"$transfer_output_files"/
+#cp -r ../model_weight/cross_validation_keck ~/"$transfer_output_files"/
+
+exit $pyexit

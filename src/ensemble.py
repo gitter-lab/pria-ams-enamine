14db1f88e184ff11b3b1a19c1f6e5bae7e2ae38a
chao1224 <shengchao.hacker@gmail.com>
2019-01-04 21:40:41 -0600

    Update ensemble

--- src/ensemble.py
+++ src/ensemble.py
@@ -13,7 +13,7 @@ from deep_regression import SingleRegression
 from util import output_classification_result
 
 
-def construct_data(conf, file_list):
+def construct_training_data(conf, file_list):
     X_train = []
     K = len(file_list)
     label_name_list = conf['label_name_list']
@@ -21,7 +21,7 @@ def construct_data(conf, file_list):
     for running_index in range(K):
         test_file_list = file_list[running_index: running_index+1]
         test_pd = read_merged_data(test_file_list)
-        X_test, _ = extract_feature_and_label(test_pd,
+        feature, _ = extract_feature_and_label(test_pd,
                                               feature_name='1024 MorganFP Radius 2',
                                               label_name_list=label_name_list)
         X_train_current_round = []
@@ -33,7 +33,7 @@ def construct_data(conf, file_list):
             with open(config_json_file, 'r') as f:
                 task_conf = json.load(f)
             task = task_module(conf=task_conf)
-            y_pred = task.predict_with_existing(X_test, model_weight_file)
+            y_pred = task.predict_with_existing(feature, model_weight_file)
             print y_pred.shape
             X_train_current_round.append(y_pred)
 
@@ -49,6 +49,32 @@ def construct_data(conf, file_list):
                                            feature_name='1024 MorganFP Radius 2',
                                            label_name_list=label_name_list)
     return X_train, y_train
+
+
+def construct_test_data(conf, file_list):
+    label_name_list = conf['label_name_list']
+    test_pd = read_merged_data(file_list)
+    feature, y_test = extract_feature_and_label(test_pd,
+                                                feature_name='1024 MorganFP Radius 2',
+                                                label_name_list=label_name_list)
+
+    running_index = 4
+    X_test_temp = []
+    for model, model_conf in conf['models'].items():
+        print('Loading {} ......'.format(model))
+        task_module = globals()[model_conf['task_module']]
+        config_json_file = model_conf['config_json_file']
+        model_weight_file = model_conf['model_weight'].format(running_index)
+        with open(config_json_file, 'r') as f:
+            task_conf = json.load(f)
+        task = task_module(conf=task_conf)
+        y_pred = task.predict_with_existing(feature, model_weight_file)
+        print y_pred.shape
+        X_test_temp.append(y_pred)
+
+    X_test = np.concatenate(X_test_temp, axis=1)
+
+    return X_test, y_test
 
 
 class Ensemble:
@@ -82,8 +108,6 @@ class Ensemble:
         y_pred_on_train = reshape_data_into_2_dim(model.predict_proba(X_train)[:, 1])
         if X_test is not None:
             y_pred_on_test = reshape_data_into_2_dim(model.predict_proba(X_test)[:, 1])
-        else:
-            y_pred_on_test = None
 
         output_classification_result(y_train=y_train, y_pred_on_train=y_pred_on_train,
                                      y_val=None, y_pred_on_val=None,
@@ -108,21 +132,21 @@ class Ensemble:
 def demo_ensemble():
     conf = {
         'models': {
-            'random_forest_classification': {
-                'task_module': 'RandomForestClassification',
-                'config_json_file': '../config/random_forest_classification/139.json',
-                'model_weight': '../model_weight/random_forest_classification/random_forest_classification_139_{}.pkl'
-            },
-            'xgboost_classification': {
-                'task_module': 'XGBoostClassification',
-                'config_json_file': '../config/xgboost_classification/140.json',
-                'model_weight': '../model_weight/xgboost_classification/xgboost_classification_140_{}.pkl'
-            },
-            'xgboost_regression': {
-                'task_module': 'XGBoostRegression',
-                'config_json_file': '../config/xgboost_regression/187.json',
-                'model_weight': '../model_weight/xgboost_regression/xgboost_regression_187_{}.pkl'
-            },
+            # 'random_forest_classification': {
+            #     'task_module': 'RandomForestClassification',
+            #     'config_json_file': '../config/random_forest_classification/139.json',
+            #     'model_weight': '../model_weight/random_forest_classification/random_forest_classification_139_{}.pkl'
+            # },
+            # 'xgboost_classification': {
+            #     'task_module': 'XGBoostClassification',
+            #     'config_json_file': '../config/xgboost_classification/140.json',
+            #     'model_weight': '../model_weight/xgboost_classification/xgboost_classification_140_{}.pkl'
+            # },
+            # 'xgboost_regression': {
+            #     'task_module': 'XGBoostRegression',
+            #     'config_json_file': '../config/xgboost_regression/187.json',
+            #     'model_weight': '../model_weight/xgboost_regression/xgboost_regression_187_{}.pkl'
+            # },
             'single_deep_classification': {
                 'task_module': 'SingleClassification',
                 'config_json_file': '../config/single_deep_classification/328.json',
@@ -138,26 +162,22 @@ def demo_ensemble():
             'ratio_list': [0.02, 0.01, 0.0015, 0.001]
         },
         'random_seed': 1337,
-        'label_name_list': ['PriA-SSB AS Activity', 'PriA-SSB AS % inhibition (Primary Median)']
+        'label_name_list': ['PriA-SSB AS Activity'] # 'PriA-SSB AS % inhibition (Primary Median)'
     }
 
     # specify dataset
     K = 5
+    K = 3
     directory = '../datasets/keck_pria/fold_{}.csv'
     file_list = []
     for i in range(K):
         file_list.append(directory.format(i))
-    file_list = np.array(file_list)
-    X_train, y_train = construct_data(conf, file_list)
-    y_train = y_train[:, 0]
+    training_file_list = np.array(file_list)
+    X_train, y_train = construct_training_data(conf, training_file_list)
     print('Consructed data: {}, {}'.format(X_train.shape, y_train.shape))
 
-    label_name_list = conf['label_name_list']
-    test_pd = read_merged_data(['../datasets/keck_pria/fold_8.csv', '../datasets/keck_pria/fold_9.csv'])
-    X_test, y_test = extract_feature_and_label(test_pd,
-                                               feature_name='1024 MorganFP Radius 2',
-                                               label_name_list=label_name_list)
-    y_test = y_test[:, 0]
+    test_file_list = ['../datasets/keck_pria/fold_8.csv', '../datasets/keck_pria/fold_9.csv']
+    X_test, y_test = construct_test_data(conf, test_file_list)
     print'Test data {}, {}'.format(X_test.shape, y_test.shape)
 
     secondary_layer_model = Ensemble(conf=conf)

5a5fdf97b0e17bbf7888055b1efeb898dac1a665
chao1224 <shengchao.hacker@gmail.com>
2019-01-24 12:49:24 -0600

    Update ensemble script: save ensemble model weights

--- src/run_ensemble.sh
+++ src/run_ensemble.sh
@@ -4,6 +4,6 @@ for i in {0..12}; do
     echo $i
     KERAS_BACKEND=theano \
     python ensemble.py \
-    --weight_file=temp.pt \
+    --weight_file=../output/ensemble/ensemble_$i.pt \
     --config_json_file=../config/ensemble/"$i".json > ../output/ensemble/"$i".out
 done

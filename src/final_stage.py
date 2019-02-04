b917a7e5603b9580313c3973c712aa2084a49899
chao1224 <shengchao.hacker@gmail.com>
2019-02-04 09:26:11 -0600

    Add script for the final stage

---
+++ src/final_stage.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+
+import rdkit
+from rdkit import Chem
+from rdkit.Chem import AllChem
+import numpy as np
+import json
+from xgboost_classification import XGBoostClassification
+
+
+if __name__ == '__main__':
+    test_file = '../datasets/emolecules.smi'
+
+    with open(test_file) as f:
+        lines = f.readlines()
+    print('{} lines in all'.format(len(lines)))
+
+    fingerprints_list = []
+    FP_radius = 2
+    FP_size = 1024
+
+    smiles_list = []
+    for idx,line in enumerate(lines):
+        line_ = line.strip().split(' ')
+        smiles = line_[0]
+        smiles_list.append(smiles)
+        mol = Chem.MolFromSmiles(smiles)
+        fingerprints = AllChem.GetMorganFingerprintAsBitVect(mol, radius=FP_radius, nBits=FP_size).ToBitString()
+        fingerprints = list(fingerprints)
+        fingerprints_list.append(fingerprints)
+
+    fingerprints_list = np.array(fingerprints_list)
+    print(fingerprints_list.shape)
+
+    config_json_file = '../config/xgboost_classification/140.json'
+    weight_file = '../model_weight/xgboost_classification/xgboost_classification_140_4.pkl'
+
+    with open(config_json_file, 'r') as f:
+        task_conf = json.load(f)
+    xgboost_model = XGBoostClassification(conf=task_conf)
+    model = xgboost_model.load_model(weight_file)
+    pred_values = model.predict_proba(fingerprints_list)[:, 1]
+    print('shape: {},\tfirst 10 values: {}'.format(pred_values.shape, pred_values[:10]))
+    print('over 0.1 has {}/{}'.format(sum(pred_values>0.1), len(pred_values)))
+
+    for smiles,pred_value in zip(smiles_list, pred_values):
+        print('{}\t{}'.format(smiles, pred_value))

6cb229410d250e34147d8e89421829464ffcd90e
chao1224 <shengchao.hacker@gmail.com>
2019-02-15 11:13:26 -0600

    Update final stage

--- src/final_stage.py
+++ src/final_stage.py
@@ -1,55 +1,109 @@
 from __future__ import print_function
 
-import rdkit
-from rdkit import Chem
-from rdkit.Chem import AllChem
-import numpy as np
 import json
+import argparse
+import numpy as np
+import pandas as pd
+from function import *
 from xgboost_classification import XGBoostClassification
 
 
-if __name__ == '__main__':
-    test_file = '../datasets/emolecules.smi'
+def train():
+    K = 10
+    directory = '../datasets/keck_pria/fold_{}.csv'
+    file_list = []
+    for i in range(K):
+        file_list.append(directory.format(i))
+    file_list = np.array(file_list)
 
-    with open(test_file) as f:
-        lines_ = f.readlines()
-    lines_ = np.array(lines_)
-    print('{} lines in all'.format(len(lines_)))
+    with open(config_json_file, 'r') as f:
+        conf = json.load(f)
+    label_name_list = conf['label_name_list']
+    print('label_name_list ', label_name_list)
 
-    FP_radius = 2
-    FP_size = 1024
+    val_index = [9]
+    complete_index = np.arange(10)
+    train_index = filter(lambda x: x not in val_index, complete_index)
 
-    handler = open('./final_stage_predicted.out', 'w')
-    idx_array = np.arange(len(lines_))
-    idx_splitted_array = np.array_split(idx_array, 50)
-    for idx_ in idx_splitted_array:
-        lines = lines_[idx_]
-        print(len(idx_), '\t', len(lines))
+    train_file_list = file_list[train_index]
+    val_file_list = file_list[val_index]
 
-        smiles_list = []
-        fingerprints_list = []
-        for idx,line in enumerate(lines):
-            line_ = line.strip().split(' ')
-            smiles = line_[0]
-            smiles_list.append(smiles)
-            mol = Chem.MolFromSmiles(smiles)
-            fingerprints = AllChem.GetMorganFingerprintAsBitVect(mol, radius=FP_radius, nBits=FP_size).ToBitString()
-            fingerprints = list(fingerprints)
-            fingerprints_list.append(fingerprints)
+    print('train files ', train_file_list)
+    print('val files ', val_file_list)
 
+    train_pd = filter_out_missing_values(read_merged_data(train_file_list), label_list=label_name_list)
+    val_pd = filter_out_missing_values(read_merged_data(val_file_list), label_list=label_name_list)
+
+    X_train, y_train = extract_feature_and_label(train_pd,
+                                                 feature_name='1024 MorganFP Radius 2',
+                                                 label_name_list=label_name_list)
+    X_val, y_val = extract_feature_and_label(val_pd,
+                                             feature_name='1024 MorganFP Radius 2',
+                                             label_name_list=label_name_list)
+    print('done data preparation')
+
+    print('X_train\t', X_train.shape)
+    print('y_train\t', y_train.shape)
+
+    task = XGBoostClassification(conf=conf)
+    task.train_and_predict(X_train, y_train, X_val, y_val, None, None, weight_file)
+    return
+
+
+def predict():
+    with open(config_json_file, 'r') as f:
+        task_conf = json.load(f)
+    xgboost_model = XGBoostClassification(conf=task_conf)
+    model = xgboost_model.load_model(weight_file)
+
+    handler = open('../output/final_stage/{}.out'.format(target), 'w')
+
+    for count in range(50):
+        df = pd.read_csv('../datasets/{}/{}.csv.gz'.format(target, count))
+        print('{}\tshape:{}'.format(count, df.shape))
+
+        def apply(x):
+            x = x.replace('[', '')
+            x = x.replace(']', '')
+            x = x.replace(',', '')
+            x = x.replace(' ', '')
+            x = x.replace('\'', '')
+            return list(x)
+
+        smiles_list = df['smiles'].tolist()
+        fingerprints_list = df['fingerprints'].tolist()
+        fingerprints_list = map(lambda x: apply(x), fingerprints_list)
+        # print(fingerprints_list[0], len(fingerprints_list[0]))
         fingerprints_list = np.array(fingerprints_list)
+        fingerprints_list= fingerprints_list.astype(float)
         print(fingerprints_list.shape)
 
-        config_json_file = '../config/xgboost_classification/140.json'
-        weight_file = '../model_weight/xgboost_classification/xgboost_classification_140_4.pkl'
-
-        with open(config_json_file, 'r') as f:
-            task_conf = json.load(f)
-        xgboost_model = XGBoostClassification(conf=task_conf)
-        model = xgboost_model.load_model(weight_file)
         pred_values = model.predict_proba(fingerprints_list)[:, 1]
         print('shape: {},\tfirst 10 values: {}'.format(pred_values.shape, pred_values[:10]))
-        print('over 0.1 has {}/{}'.format(sum(pred_values>0.1), len(pred_values)))
+        print('over 0.1 has {}/{}'.format(sum(pred_values > 0.1), len(pred_values)))
 
-        for smiles,pred_value in zip(smiles_list, pred_values):
+        for smiles, pred_value in zip(smiles_list, pred_values):
             print('{}\t{}'.format(smiles, pred_value), file=handler)
+        print()
+    return
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--target', default='aldrich')
+    parser.add_argument('--config_json_file', default='../config/xgboost_classification/140.json')
+    parser.add_argument('--weight_file', default='../model_weight/final_stage/xgboost_classification_140.pkl')
+    parser.add_argument('--mode', default='training')
+
+    args = parser.parse_args()
+    target = args.target
+    config_json_file = args.config_json_file
+    weight_file = args.weight_file
+    mode = args.mode
+
+    if mode == 'training':
+        print('Start Training.')
+        train()
+    elif mode == 'prediction':
+        print('Start Predictiong.')
+        predict()

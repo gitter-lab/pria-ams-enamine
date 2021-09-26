63c233a1e2366a0e95d1d895f324bbf7a1097a7b
Malnammi <alnammi2009@gmail.com>
2021-09-26 22:51:14 +0300

    Delete sample.py

--- datasets/keck_pria_test/sample.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-if __name__ == '__main__':
-    for i in range(10):
-        dataframe = pd.read_csv('../keck_pria/fold_{}.csv'.format(i), nrows=1000)
-        dataframe.to_csv('fold_{}.csv'.format(i), index=None)
